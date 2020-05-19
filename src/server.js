d30a172f3ae5e4f589773e01407951dc10956a12
Marthijn Batlajeri <marthijn.batlajeri@marthijnbnb.objective.com>
2020-05-19 14:42:49 +1200

    All Api using MongoDB now

--- src/server.js
+++ src/server.js
@@ -20,31 +20,47 @@ const app = express();
 //   },
 // }
 
-app.use(bodyParser.json());
-
-app.get('/api/articles/:name', async (req, res) => {
+const withDB = async (operations, res) => {
   try {
-    const articleName = req.params.name;
-
     const client = await MongoClient.connect('mongodb://localhost:27017', { useNewUrlParser: true });
-    // res.status(200).json({ message: 'here'});
     const db = client.db('my-blog');
 
-    const articleInfo = await db.collection('articles').findOne({ name: articleName });
-    res.status(200).json(articleInfo);
+    await operations(db);
 
     client.close();
   } catch (error) {
     res.status(500).json({ message: 'Failed to connect to db ', error })
   }
+}
+
+app.use(bodyParser.json());
+
+app.get('/api/articles/:name', async (req, res) => {
+    withDB ( async (db) => {
+      const articleName = req.params.name;
+
+      const articleInfo = await db.collection('articles').findOne({ name: articleName });
+      res.status(200).json(articleInfo);
+    }, res);
 
 })
 
-app.post('/api/articles/:name/upvote', (req, res) => {
-  const articleName = req.params.name;
-
-  articlesInfo[articleName].upvotes += 1;
-  res.status(200).send(`${articleName} now has ${articlesInfo[articleName].upvotes} upvotes`);
+app.post('/api/articles/:name/upvote', async (req, res) => {
+  withDB (async (db) => {
+    const articleName = req.params.name;
+    // Retrieve article info
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    // Increment upvote value
+    await db.collection('articles').updateOne( { name: articleName }, {
+      '$set' : {
+        upvote: articleInfo.upvote + 1,
+      }
+    });
+    // Retrtieve updated artle info
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    // Send updates article info back to the MongoClient
+    res.status(200).json(updatedArticleInfo);
+  }, res);
 })
 
 app.post('/api/articles/:name/add-comment', (req, res) => {
@@ -52,9 +68,19 @@ app.post('/api/articles/:name/add-comment', (req, res) => {
   const articleName = req.params.name;
   const timestamp = Date.now();
 
-  articlesInfo[articleName].comments.push({ username, text, timestamp });
-  res.status(200).send(articlesInfo[articleName]);
-})
+  withDB( async (db) => {
+    const articleInfo = await db.collection('articles').findOne({ name: articleName });
+    await db.collection('articles').updateOne( { name: articleName }, {
+      '$set' : {
+        comments: articleInfo.comments.concat({username, text}),
+      }
+    });
+    // Retrtieve updated artle info
+    const updatedArticleInfo = await db.collection('articles').findOne({ name: articleName });
+    // Send updates article info back to the MongoClient
+    res.status(200).json(updatedArticleInfo);
+  }, res)
+});
 
 app.get('/hello', (req, res) => res.send("Hello! (Get Response)"));
 app.get('/hello/:name', (req, res) => res.send(`Hello ${req.params.name}!`))
